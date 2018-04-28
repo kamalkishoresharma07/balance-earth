2187f4aadbcb3e2326f559974dc921612402c35d
sadanand123 <sadanand@incaendo.com>
2018-04-28 19:04:21 +0530

    new magento file

---
+++ app/etc/env.php
@@ -0,0 +1,49 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'abacus'
+    ],
+    'crypt' => [
+        'key' => 'e6ab054b00db180178e40b2a530e635c'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'megento2',
+                'username' => 'root',
+                'password' => 'redhat',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'translate' => 1,
+        'config_webservice' => 1
+    ],
+    'install' => [
+        'date' => 'Fri, 27 Apr 2018 09:04:52 +0000'
+    ]
+];
